3807f95ab1c352f78410cd37f19af38bd6ffddc3
VeereshDodamani <135006390+VeereshDodamani@users.noreply.github.com>
2024-11-12 06:39:15 +0530

    1st ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -1,1 +1,14 @@
+import os
 
+import zipfile
+
+import pandas as pd
+
+class ZipDataIngestor(DataIngestor):
+  def ingest(self, file_path: str) -> pd.DataFrame:
+    ""Extract a .zip file and returns the content as a panda DataFrame.""
+    if not file_path.endswith(".zip"):
+      raise ValueError("The provided file is not in zip format.")
+
+    with zipfile.ZipFile(file_path,"r") as zip_red:
+      zip_ref.extractall("extracted_data")
