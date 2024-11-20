85f26795fdd449f7f36617ff8fd575bddb48ba0b
VeereshDodamani <135006390+VeereshDodamani@users.noreply.github.com>
2024-11-20 11:03:51 +0530

    Create basic_data_inspection.py

---
+++ analyze_src/basic_data_inspection.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+
+# Abstract Base Class for Data Inspection Strategies
+
+class DataInspectionStrategy(ABC):
+    @abstractmethod
+    def inspect(self, df: pd.DataFrame):
+        """
+        Perform a specific type of data inspection.
+
+        Parameters:
+        df (pd.DataFrame): The dataframe on which the inspection is to be performed.
+
+        Returns:
+        None: This method prints the inspection results directly.
+        """
+        pass
