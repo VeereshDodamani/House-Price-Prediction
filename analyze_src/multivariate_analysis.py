445dc82c2f6d8060181dbfdb707896c360ad82f2
VeereshDodamani <135006390+VeereshDodamani@users.noreply.github.com>
2024-11-29 12:05:48 +0530

    Create multivariate_analysis.py

---
+++ analyze_src/multivariate_analysis.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# Abstract Base Class for Multivariate Analysis
+
+# This class defines a template for performing multivariate analysis.
+# Subclasses can override specific steps like correlation heatmap and pair plot generation.
+class MultivariateAnalysisTemplate(ABC):
+    def analyze(self, df: pd.DataFrame):
+        """
+        Perform a comprehensive multivariate analysis by generating a correlation heatmap and pair plot.
+
+        Parameters:
+        df (pd.DataFrame): The dataframe containing the data to be analyzed.
+
+        Returns:
+        None: This method orchestrates the multivariate analysis process.
+        """
+        self.generate_correlation_heatmap(df)
