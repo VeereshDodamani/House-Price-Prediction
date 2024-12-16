96462a4d2f815bfe383af2f2bf4be34ba0562325
VeereshDodamani <135006390+VeereshDodamani@users.noreply.github.com>
2024-12-16 13:13:09 +0530

    Create development_pipeline.py

---
+++ pipelines /development_pipeline.py
@@ -0,0 +1,19 @@
+import os
+
+from pipelines.training_pipeline import ml_pipeline
+from steps.dynamic_importer import dynamic_importer
+from steps.predictor import predictor
+from zenml import pipeline
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+
+requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+
+@pipeline
+def continuous_deployment_pipeline():
+    """Run a training job and deploy an MLflow model deployment."""
+    # Run the training pipeline
+    trained_model = ml_pipeline()  # No need for is_promoted return value anymore
+
+    # (Re)deploy the trained model
+    mlflow_model_deployer_step(workers=3, deploy_decision=True, model=trained_model)
